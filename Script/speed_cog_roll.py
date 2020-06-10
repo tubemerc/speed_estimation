0cb0723132c192fe853cf8c827031f21e655f973
tubemerc <tubemerc+1@gmail.com>
2020-06-10 12:18:22 +0900

    [FIX]

--- Script/speed_cog_roll.py
+++ Script/speed_cog_roll.py
@@ -3,13 +3,14 @@
 '''
 ToDo:
     - 路線変更したのでロール角算出に適した形にする
+    - ﾏｰｶｰを色や形で絞る
 '''
 
 import cv2
 import numpy as np
 import time
 from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 
 set_parameter = {
                  "input_video" : 0,
@@ -19,11 +20,12 @@ set_parameter = {
                  "coefficient" : 1,
                  "areasize"    : 0.0008,
                  "height"      : 240,
-                 "width"       : 320
+                 "width"       : 320,
+                 "flag_rec"    : True
                  }
 
 
-class Horizontal_speed__estimation_by_com:
+class Calculating_roll_from_com:
     # input_video : 入力映像. device number or file path.
     # th_binary_1 : 2値化閾値1.
     # th_binary_2 : 2値化閾値2.
@@ -32,18 +34,21 @@ class Horizontal_speed__estimation_by_com:
     # areasize    : 輪郭の採用範囲.
     # height      : 入力画像のリサイズ高さ.
     # width       : 入力画像のリサイズ幅.
+    # flag_rec    : 録画設定.
     
     def __init__(self, input_video,
                        th_binary_1, th_binary_2, th_binary_3,
                        coefficient,
                        areasize,
-                       height=None, width=None):
-        self.th_binary_1  = th_binary_1
-        self.th_binary_2  = th_binary_2
-        self.th_binary_3  = th_binary_3
-        self.coefficient  = coefficient
-        self.height       = height
-        self.width        = width
+                       height=None, width=None,
+                       flag_rec=False):
+        self.th_binary_1 = th_binary_1
+        self.th_binary_2 = th_binary_2
+        self.th_binary_3 = th_binary_3
+        self.coefficient = coefficient
+        self.height      = height
+        self.width       = width
+        self.flag_rec    = flag_rec
         
         self.org = cv2.VideoCapture(input_video)
         self.end_flag, self.frame_org = self.org.read()
@@ -69,33 +74,42 @@ class Horizontal_speed__estimation_by_com:
         cv2.namedWindow("org")
         cv2.namedWindow("bin")
     
-        # 録画
-        fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-        self.writer = cv2.VideoWriter('./outtest.mp4', fmt, 30.0, (self.width ,self.height))
+        if self.flag_rec == True:
+            fmt = cv2.VideoWriter_fourcc("m", "p", "4", "v")
+            self.writer = cv2.VideoWriter("./video_roll.mp4",
+                                          fmt,
+                                          30.0,
+                                          (self.width ,self.height))
     
     
-    def speed_estimation(self):
+    def roll_estimation(self):
         com_sum  = []
         time_cs  = []
         cnt_cs   = []
         vel      = []
         time_vel = []
         cnt_vel  = []
-        self.cnt = 1
         
-        print("Speed estimation start.")
+        cnt = 1
+        x_o = int(self.width/2)
+        y_o = int(self.height/2)
+        FONT = cv2.FONT_HERSHEY_SIMPLEX
+        MARKER = cv2.MARKER_CROSS
+        LINE = cv2.LINE_4
         time_0 = time.time()
         
+        print("Speed estimation start.")
         while self.end_flag == True:
+            dt = time.time() - time_0
             self.frame_org = cv2.resize(self.frame_org,
                                         dsize=(self.width, self.height))
             frame_bin = cv2.cvtColor(self.frame_org, cv2.COLOR_BGR2GRAY)
             _, frame_bin = cv2.threshold(frame_bin, self.th_binary_1,
                                                     self.th_binary_2,
                                                     self.th_binary_3)
-            self.dt = time.time() - time_0
-            
-            # 重心座標算出
+            '''
+            重心座標算出
+            '''
             contours, _ = cv2.findContours(frame_bin, 
                                            cv2.RETR_EXTERNAL, 
                                            cv2.CHAIN_APPROX_TC89_L1)
@@ -108,8 +122,7 @@ class Horizontal_speed__estimation_by_com:
 #                center = (int(x), int(y))
 #                radius = int(radius)
 #                cv2.circle(self.frame_org, center, radius, (255, 0, 0), 2)
-#                    
-            #print(_)
+            
             com_org = []
             for c in contours:
                 _moment = cv2.moments(c)
@@ -135,16 +148,16 @@ class Horizontal_speed__estimation_by_com:
                 com_sum_x = int(sum(_com_x)/_len_x)
                 com_sum_y = int(sum(_com_y)/_len_y)
                 com_sum.append((com_sum_x, com_sum_y))  # com_sum保存
-                time_cs.append(self.dt)
-                cnt_cs.append(self.cnt)
+                time_cs.append(dt)
+                cnt_cs.append(cnt)
                 # velocity算出.
-                if self.cnt != 1:
+                if cnt != 1:
                     self._vel_x = (com_sum[-2][0]-com_sum_x)*self.coefficient
                     self._vel_y = (com_sum[-2][1]-com_sum_y)*self.coefficient
                     vel.append((self._vel_x, self._vel_y))  # vel保存.
-                    time_vel.append(self.dt)
-                    cnt_vel.append(self.cnt)
-                    #print(self.cnt, ": Velocity :", vel[-1])
+                    time_vel.append(dt)
+                    cnt_vel.append(cnt)
+                    #print(cnt, ": Velocity :", vel[-1])
                     # com_sum軌跡描写
                     cv2.arrowedLine(self.frame_org,
                                     (com_sum[-2][0],com_sum[-2][1]),
@@ -154,31 +167,28 @@ class Horizontal_speed__estimation_by_com:
                                     line_type = 8,
                                     shift     = 0,
                                     tipLength = 0.1)
-                    
-                    #self.quaternion_estimation()
                 
                 if(_len_x*_len_y != 0):  # com_sum描写
                     self.frame_org = cv2.drawMarker(self.frame_org,
                                                     (com_sum_x, com_sum_y),
                                                     color      = (0,0,255),
-                                                    markerType = cv2.MARKER_CROSS,
+                                                    markerType = MARKER,
                                                     markerSize = 20,
                                                     thickness  = 2)
-                self.cnt += 1
+                cnt += 1
                 
-                # ロール角
-                x_o = int(self.width/2)
-                y_o = int(self.height/2)
+                '''
+                ロール角算出
+                '''
                 x = com_sum_x - x_o
                 y = y_o - com_sum_y
                 radius = pow(x**2 + y**2, 1/2)
                 angle = np.arctan2(y, x) * 180/3.14159265
-                #print(radius)
-                print(angle)
+                
                 self.frame_org = cv2.drawMarker(self.frame_org,
                                                 (x_o, y_o),
                                                 color      = (0,0,255),
-                                                markerType = cv2.MARKER_CROSS,
+                                                markerType = MARKER,
                                                 markerSize = 20,
                                                 thickness  = 2)
                 cv2.arrowedLine(self.frame_org,
@@ -191,28 +201,17 @@ class Horizontal_speed__estimation_by_com:
                                 tipLength = 0.1)
                 cv2.putText(self.frame_org,
                            "angle:"+"{:.4g}".format(angle),
-                           (0, 20),
-                           cv2.FONT_HERSHEY_SIMPLEX,
-                           0.8,
-                           (255, 0, 0),
-                           2,
-                           cv2.LINE_4)
+                           (0, 20), FONT, 0.8, (255, 0, 0), 2, LINE)
                 cv2.putText(self.frame_org,
                            "radius:"+"{:.4g}".format(radius),
-                           (0, 50),
-                           cv2.FONT_HERSHEY_SIMPLEX,
-                           0.8,
-                           (255, 0, 0),
-                           2,
-                           cv2.LINE_4)
+                           (0, 50), FONT, 0.8, (255, 0, 0), 2, LINE)
                 
-            
-            cv2.drawContours(self.frame_org, contours, -1, (0,255,0), 1)
+                cv2.drawContours(self.frame_org, contours, -1, (0,255,0), 1)
             cv2.imshow("org", self.frame_org)
             cv2.imshow("bin", frame_bin)
             
-            # 録画
-            self.writer.write(self.frame_org)
+            if self.flag_rec == True:
+                self.writer.write(self.frame_org)
             
             self.end_flag, self.frame_org = self.org.read()
             
@@ -250,7 +249,8 @@ class Horizontal_speed__estimation_by_com:
         plt.ylabel("velocity_y[pixels/frame]")
         plt.grid()
         plt.show()
-        
+
+
 if __name__ == "__main__":
-    hse_c = Horizontal_speed__estimation_by_com(**set_parameter)
-    hse_c.speed_estimation()
+    crfc = Calculating_roll_from_com(**set_parameter)
+    crfc.roll_estimation()
